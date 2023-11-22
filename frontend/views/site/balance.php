d0a26dd0f41f22664e76a41be93dea4f2ef6151f
Qalandar Dev <uzwebdevolopersn2@gmail.com>
2023-11-22 15:28:58 +0500

    Update balance.php

--- frontend/views/site/balance.php
+++ frontend/views/site/balance.php
@@ -33,7 +33,7 @@ use yii\web\View;
 </style>
 <div class="card">
     <div class="card-header">
-        <span class="text-left">Joriy Balans Hisobni to'ldirish uchun Telegram orqali <a style="color:yellow; border: 2px solid white" href='https://texnolog2021.t.me'>@texnolog2021</a></span>
+        <span class="text-left">Joriy Balans Hisobni to'ldirish uchun Telegram orqali <a style="color:yellow; border: 2px solid white" href='https://texnolog2024.t.me'>@texnolog2024</a></span>
         <span class="balance-currency" style="float: right;"><?= $user->balanceAsDecimal() ?> UZS</span>
     </div>
     <div class="card-body">
